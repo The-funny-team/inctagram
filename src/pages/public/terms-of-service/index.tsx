0b62dc0820ceb974db3618bc28e7da8a06e99aa4
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2023-11-16 08:17:45 +0100

    feat: add terms-of-service page placeholder

---
+++ src/pages/public/terms-of-service/index.tsx
@@ -0,0 +1,8 @@
+import {getRootLayout} from '@/common/components'
+
+const TermsOfService = () => {
+  return <h1>Terms Of Service</h1>
+}
+
+TermsOfService.getLayout = getRootLayout
+export default TermsOfService
