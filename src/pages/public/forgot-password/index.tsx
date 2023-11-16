75458fb7a93144f856d05b96568e640f27cd6cb8
user <y.dubovitsky@gmail.com>
2023-11-16 12:28:00 +0300

    I1-39 feat: ForgotPassword page add

---
+++ src/pages/public/forgot-password/index.tsx
@@ -0,0 +1,8 @@
+import {getRootLayout} from '@/common/components'
+
+const ForgotPassword = () => {
+    return <h1>ForgotPassword</h1>
+}
+
+ForgotPassword.getLayout = getRootLayout
+export default ForgotPassword
