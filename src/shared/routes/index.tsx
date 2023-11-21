491166267cc0964fece520e46434c46ab8396024
y-dubovitsky <pXmJ5M9dvP>
2023-11-21 23:56:42 +0300

    feat: shared => rootes constant add

---
+++ src/shared/routes/index.tsx
@@ -0,0 +1,10 @@
+const ROUTES_URL = {
+  FORGOT_PASSWORD: '/forgot-password',
+  PRIVACY_POLICY: '/privacy-policy',
+  SIGN_IN: '/sign-in',
+  SIGN_UP: '/sign-up',
+  TERMS_OF_SERVICE: '/terms-of-service',
+  USER_PROFILE: '/user-profile',
+}
+
+export default ROUTES_URL
