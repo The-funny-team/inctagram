292a217b72e8beb389f1a35f7c46145e2ee92eca
vladnen97 <darkdescent1991@gmail.com>
2023-11-26 14:44:58 +0300

    I1-55-1 feat: add props to svg

--- src/shared/assets/icons/ArrowDown.tsx
+++ src/shared/assets/icons/ArrowDown.tsx
@@ -8,6 +8,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
     viewBox={'0 0 24 24'}
     width={'24'}
     xmlns={'http://www.w3.org/2000/svg'}
+    {...props}
   >
     <path
       d={
