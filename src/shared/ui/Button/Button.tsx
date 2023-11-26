686655fd4b3884a9a7635f80e29cf5d3ebacc336
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2023-11-26 11:44:57 +0100

    I1-56 feat: add button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,32 @@
+import React, { ComponentPropsWithoutRef, ElementType, FC, ReactNode } from 'react'
+
+import clsx from 'clsx'
+
+import s from './Button.module.scss'
+
+type Props<T extends ElementType = 'button'> = {
+  as?: T
+  children: ReactNode
+  className?: string
+  fullWidth?: boolean
+  variant?: 'link' | 'primary' | 'secondary' | 'tertiary'
+} & ComponentPropsWithoutRef<T>
+
+export const Button: FC<Props> = <T extends ElementType = 'button'>(
+  props: Props<T> & Omit<ComponentPropsWithoutRef<T>, keyof Props<T>>
+) => {
+  const {
+    as: Component = 'button',
+    children,
+    className,
+    fullWidth = true,
+    variant = 'primary',
+    ...rest
+  } = props
+
+  return (
+    <Component className={clsx(s[variant], fullWidth ? s.fullWidth : '', className)} {...rest}>
+      {children}
+    </Component>
+  )
+}
