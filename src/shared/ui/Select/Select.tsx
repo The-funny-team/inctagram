8d668a0ac9b87f206171b5bfbbcb955792264fb4
vladnen97 <darkdescent1991@gmail.com>
2023-11-26 14:43:32 +0300

    I1-55-1 feat: wrap in forward ref;  use label prop as id

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,12 +1,12 @@
-import { FC, ReactElement } from 'react'
+import { ElementRef, ReactElement, forwardRef } from 'react'
 
 import * as SelectRadix from '@radix-ui/react-select'
 import { clsx } from 'clsx'
 
+import Down from '../../assets/icons/ArrowDown'
+
 // eslint-disable-next-line perfectionist/sort-imports
 import s from './Select.module.scss'
-
-import Down from '../../assets/icons/ArrowDown'
 
 type Props = {
   disabled?: boolean
@@ -18,52 +18,43 @@ type Props = {
   value?: string
 }
 
-export const Select: FC<Props> = ({
-  disabled,
-  label,
-  name,
-  onValueChange,
-  options,
-  placeholder,
-  value,
-}) => {
-  return (
-    <div>
-      {label && (
-        <label className={clsx(s.label)} htmlFor={name}>
-          {label}
-        </label>
-      )}
-      <SelectRadix.Root
-        disabled={disabled}
-        name={name}
-        onValueChange={onValueChange}
-        open
-        value={value}
-      >
-        <SelectRadix.Trigger asChild className={clsx(s.trigger)} tabIndex={0}>
-          <div>
+export const Select = forwardRef<ElementRef<typeof SelectRadix.Trigger>, Props>(
+  ({ disabled, label, name, onValueChange, options, placeholder, value }, ref) => {
+    return (
+      <div>
+        {label && (
+          <label className={clsx(s.label)} htmlFor={label}>
+            {label}
+          </label>
+        )}
+        <SelectRadix.Root
+          disabled={disabled}
+          name={name}
+          onValueChange={onValueChange}
+          value={value}
+        >
+          <SelectRadix.Trigger className={clsx(s.trigger)} id={label} ref={ref} tabIndex={0}>
             <SelectRadix.Value placeholder={placeholder} />
             <SelectRadix.Icon className={clsx(s.icon)}>
               <Down />
             </SelectRadix.Icon>
-          </div>
-        </SelectRadix.Trigger>
+          </SelectRadix.Trigger>
 
-        <SelectRadix.Portal>
-          <SelectRadix.Content position={'popper'} sideOffset={-1}>
-            <SelectRadix.Viewport className={clsx(s.viewport)}>
-              {options.map(el => (
-                <SelectRadix.Item className={clsx(s.item)} key={el.value} value={el.value}>
-                  <SelectRadix.ItemText asChild>
-                    <span className={clsx(s.itemText)}>{el.label}</span>
-                  </SelectRadix.ItemText>
-                </SelectRadix.Item>
-              ))}
-            </SelectRadix.Viewport>
-          </SelectRadix.Content>
-        </SelectRadix.Portal>
-      </SelectRadix.Root>
-    </div>
-  )
-}
+          <SelectRadix.Portal>
+            <SelectRadix.Content position={'popper'} sideOffset={-1}>
+              <SelectRadix.Viewport className={clsx(s.viewport)}>
+                {options.map(el => (
+                  <SelectRadix.Item className={clsx(s.item)} key={el.value} value={el.value}>
+                    <SelectRadix.ItemText asChild>
+                      <span className={clsx(s.itemText)}>{el.label}</span>
+                    </SelectRadix.ItemText>
+                  </SelectRadix.Item>
+                ))}
+              </SelectRadix.Viewport>
+            </SelectRadix.Content>
+          </SelectRadix.Portal>
+        </SelectRadix.Root>
+      </div>
+    )
+  }
+)
