9dfcd6c3c1056b94b7aac7abe3b1c95c148dab20
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-27 23:36:47 +0300

    I1-61 refactor: remove unused component properties from stories

--- src/widgets/Header/ui/Header.stories.tsx
+++ src/widgets/Header/ui/Header.stories.tsx
@@ -1,10 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import Image from 'next/image'
-
 import { Header } from './Header'
-import flagRu from '/public/flagRussia.png'
-import flagEng from '/public/flagUnitedKingdom.png'
 
 const meta = {
   component: Header,
@@ -18,39 +14,14 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-const optionsWithIcons = [
-  {
-    label: (
-      <>
-        <Image alt={'flag russian'} height={20} src={flagRu} width={20} />
-        <span>Russian</span>
-      </>
-    ),
-    value: 'ru',
-  },
-  {
-    label: (
-      <>
-        <Image alt={'flag english'} height={20} src={flagEng} width={20} />
-        <span>English</span>
-      </>
-    ),
-    value: 'en',
-  },
-]
-
 export const LoggedIn: Story = {
   args: {
     isLoggedIn: true,
-    languageOptions: optionsWithIcons,
-    languageValue: optionsWithIcons[0].value,
   },
 }
 
 export const UnLogged: Story = {
   args: {
     isLoggedIn: false,
-    languageOptions: optionsWithIcons,
-    languageValue: optionsWithIcons[0].value,
   },
 }
