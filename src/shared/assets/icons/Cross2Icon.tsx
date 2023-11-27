8187b915c7ff15f9a0dee18baaacf2b2320283f1
y-dubovitsky <pXmJ5M9dvP>
2023-11-27 20:15:57 +0300

    I1-59 fix: ref and ...props as additional props add to the Cross2Icon

--- src/shared/assets/icons/Cross2Icon.tsx
+++ src/shared/assets/icons/Cross2Icon.tsx
@@ -4,9 +4,11 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
   <svg
     fill={'none'}
     height={'14'}
+    ref={ref}
     viewBox={'0 0 14 14'}
     width={'14'}
     xmlns={'http://www.w3.org/2000/svg'}
+    {...props}
   >
     <path
       d={
