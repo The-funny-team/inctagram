3c724a7ee7d4cf7220ad1ff32a88e6f50265acac
vladnen97 <darkdescent1991@gmail.com>
2023-11-29 22:08:29 +0300

    feat: add eng locale

---
+++ locales/en.ts
@@ -0,0 +1,12 @@
+export const en = {
+  layout: {
+    header: {
+      signInBtn: 'Log in',
+      signUpBtn: 'Sign up',
+    },
+  },
+  pages: {
+    home: 'Home page',
+  },
+}
+export type LocaleType = typeof en
