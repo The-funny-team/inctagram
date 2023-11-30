c6649fdb2df5c74df1483498a11c388ca5fbd683
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2023-11-30 18:59:41 +0100

    I1-79 feat: add widget ExpiredLink

---
+++ src/widgets/ExpiredLink/ui/ExpiredLink.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+import { ExpiredTimeIcon } from '@/shared/assets'
+import { useTranslation } from '@/shared/lib/hooks'
+import { Button, Modal, Typography } from '@/shared/ui'
+
+export const ExpiredLink = () => {
+  const [showModal, setShowModal] = useState<boolean>(false)
+
+  const t = useTranslation().widgets.expiredLink
+
+  const showModalHandler = () => {
+    setShowModal(!showModal)
+  }
+
+  return (
+    <div className={s.wrapper}>
+      <div className={s.text}>
+        <Typography className={s.title} variant={'h1'}>
+          {t.title}
+        </Typography>
+        <Typography variant={'regularText16'}>{t.description}</Typography>
+      </div>
+      <div className={s.button}>
+        <Button fullWidth={false} onClick={showModalHandler}>
+          {t.btnTitle}
+          {/*Resend verification link*/}
+        </Button>
+      </div>
+      <ExpiredTimeIcon />
+      {
+        <Modal isOpen={showModal} onIsOpenChange={showModalHandler} title={'Email sent'}>
+          <p>We have resent a verification link to epam@epam.com</p>
+          <div className={s.modalButton}>
+            <Button fullWidth={false} onClick={showModalHandler} type={'button'}>
+              OK
+            </Button>
+          </div>
+        </Modal>
+      }
+    </div>
+  )
+}
