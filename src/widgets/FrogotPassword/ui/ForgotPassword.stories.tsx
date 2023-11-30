3359d08c47f4863a8c489472a975ca14d818273a
vladnen97 <darkdescent1991@gmail.com>
2023-11-30 22:58:20 +0300

    I1-76 feat: add stories for forgot password

---
+++ src/widgets/FrogotPassword/ui/ForgotPassword.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { ForgotPassword } from '@/widgets/FrogotPassword/ui/ForgotPassword'
+
+const meta = {
+  component: ForgotPassword,
+  decorators: [
+    Story => (
+      <div
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center',
+          marginTop: '80px',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+  title: 'Widgets/ForgotPassword',
+} satisfies Meta<typeof ForgotPassword>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
