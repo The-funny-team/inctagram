10822e8beeaca2aedfb754b8cb9f32290dbe0bdb
vladnen97 <darkdescent1991@gmail.com>
2023-12-01 20:50:29 +0300

    I1-76 feat: use special hook; add mock submitHandler; use transcription

--- src/widgets/FrogotPassword/ui/ForgotPassword.tsx
+++ src/widgets/FrogotPassword/ui/ForgotPassword.tsx
@@ -1,64 +1,94 @@
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-
 import { RecaptchaIcon } from '@/shared/assets'
-import { Button, Card, Checkbox, Typography } from '@/shared/ui'
-import { ForgotPasswordFormValues, forgotPasswordSchema } from '@/widgets/FrogotPassword/services'
+import { ROUTES_URL } from '@/shared/const'
+import { Button, Card, Checkbox, Modal, Typography } from '@/shared/ui'
+import { ForgotPasswordFormValues, useForgotPasswordForm } from '@/widgets/FrogotPassword/services'
 import { ControlledInput } from '@/widgets/FrogotPassword/ui/ControlledInput'
-import { zodResolver } from '@hookform/resolvers/zod'
+import Link from 'next/link'
 
 import s from './ForgotPassword.module.scss'
 
-type Props = {
-  onSubmit: (data: ForgotPasswordFormValues) => void
-}
+export const ForgotPassword = () => {
+  const {
+    control,
+    getValues,
+    handleSubmit,
+    isChecked,
+    isDisabled,
+    isOpenModal,
+    isSuccess,
+    setError,
+    setIsChecked,
+    setIsOpenModal,
+    setIsSuccess,
+    transcripton,
+  } = useForgotPasswordForm()
 
-export const ForgotPassword = ({ onSubmit }: Props) => {
-  const [isChecked, setIsChecked] = useState(false)
-  const { control, handleSubmit, watch } = useForm<ForgotPasswordFormValues>({
-    defaultValues: {
-      email: '',
-    },
-    mode: 'onBlur',
-    resolver: zodResolver(forgotPasswordSchema),
-  })
-
-  const isDisabled = !(watch('email') && isChecked)
+  const submitHandler = (data: ForgotPasswordFormValues) => {
+    new Promise((res, rej) => {
+      setTimeout(() => {
+        res(data)
+      }, 2000)
+    })
+      .then(data => {
+        console.log(data)
+        setIsSuccess(true)
+        setIsOpenModal(true)
+      })
+      .catch(() => {
+        setError('email', { message: `User with this email doesn't exist`, type: 'custom' })
+      })
+  }
 
   return (
     <Card className={s.card}>
       <Typography as={'h1'} className={s.title} variant={'h1'}>
-        Forgot Password
+        {transcripton.title}
       </Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(submitHandler)}>
         <ControlledInput
-          autoComplete={'darova'}
           control={control}
-          label={'Email'}
+          label={transcripton.inputLabel}
           name={'email'}
           placeholder={'Example@mail.com'}
           type={'text'}
         />
         <Typography className={s.caption} variant={'regularText14'}>
-          Enter your email address and we will send you further instructions
+          {transcripton.caption}
         </Typography>
-        <Button disabled={isDisabled} type={'submit'}>
-          Send Link
+        {isSuccess && (
+          <Typography className={s.captionAfterSubmit} variant={'regularText14'}>
+            {transcripton.captionAfterSubmit}
+          </Typography>
+        )}
+        <Button className={s.sendLink} disabled={isDisabled} type={'submit'}>
+          {isSuccess ? transcripton.resendLink : transcripton.sendLinkBtn}
         </Button>
       </form>
-      <Button className={s.link} fullWidth variant={'link'}>
-        Back to Sign In
+      <Button as={Link} className={s.link} fullWidth href={ROUTES_URL.SIGN_IN} variant={'link'}>
+        {transcripton.signInBtn}
       </Button>
-      <div className={s.notARobot}>
-        <Checkbox
-          checked={isChecked}
-          label={`I'm not a robot`}
-          onCheckedChange={(value: boolean) => setIsChecked(value)}
-        />
-        <span className={s.recaptcha}>
-          <RecaptchaIcon height={47} width={54} />
-        </span>
-      </div>
+      {!isSuccess && (
+        <div className={s.notARobot}>
+          <Checkbox
+            checked={isChecked}
+            label={transcripton.recaptcha}
+            onCheckedChange={(value: boolean) => setIsChecked(value)}
+          />
+          <span className={s.recaptcha}>
+            <RecaptchaIcon height={47} width={54} />
+          </span>
+        </div>
+      )}
+      <Modal isOpen={isOpenModal} onIsOpenChange={setIsOpenModal} title={transcripton.modal.title}>
+        <Typography variant={'regularText16'}>
+          {transcripton.modal.getBody(getValues('email'))}
+        </Typography>
+        <div className={s.modalBtnGroup}>
+          <Button fullWidth={false} onClick={() => setIsOpenModal(false)}>
+            OK
+          </Button>
+        </div>
+      </Modal>
     </Card>
   )
 }
