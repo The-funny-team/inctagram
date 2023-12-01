b68a5a548c718f393ec0c928d8243cf290fdc597
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2023-12-01 19:48:17 +0100

    I1-79 feat: add story widget ExpiredLink

---
+++ src/widgets/ExpiredLink/ui/ExpiredLink.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { ExpiredLink } from './ExpiredLink'
+
+const meta = {
+  component: ExpiredLink,
+  tags: ['autodocs'],
+  title: 'Widgets/ExpiredLink',
+} satisfies Meta<typeof ExpiredLink>
+
+export default meta
+type Story = StoryObj<typeof meta>
+export const Default: Story = {}
