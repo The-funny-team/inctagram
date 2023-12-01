1735c824bd417b97cef74473310bfb2199210c36
vladnen97 <darkdescent1991@gmail.com>
2023-12-01 20:43:59 +0300

    I1-76 feat: add ru locales for ForgotPassword

--- locales/ru.ts
+++ locales/ru.ts
@@ -8,6 +8,22 @@ export const ru: LocaleType = {
     },
   },
   pages: {
+    forgotPassword: {
+      caption: 'Введите вашу почту и мы вышлем вам дальнейшие иснструкции',
+      captionAfterSubmit:
+        'Письмо отправлено на почту. Если вы не получили писльмо нажмите на отправить письмо повторно',
+      emailValidationError: 'Почта должна соответствовать формату example@example.com',
+      inputLabel: 'Почта',
+      modal: {
+        getBody: (email: string) => `Мы выслали вам письмо для подтверждения почты на ${email}`,
+        title: 'Письмо отправлено',
+      },
+      recaptcha: 'Я не робот',
+      resendLink: 'Выслать Письмо Повторно',
+      sendLinkBtn: 'Отправить Письмо',
+      signInBtn: 'Вернуться на Вход',
+      title: 'Восстановить Пароль',
+    },
     home: 'Главная страница',
   },
 }
