3b0f1a11be5e0fc09f33c8d9c21768e7fff4ad95
Sergey <Zaikouski.sergey@gmail.com>
2023-12-02 10:41:25 +0200

    I1-74 : add add errors and pages translate

--- locales/en.ts
+++ locales/en.ts
@@ -1,4 +1,16 @@
 export const en = {
+  errors: {
+    signUp: {
+      emailVerification: 'The email must match the format example@example.com',
+      maxPasswordLength20: 'Maximum number of characters 20',
+      maxPasswordLength30: 'Maximum number of characters 30',
+      minPasswordLength: 'Minimum number of characters 6',
+      passwordMismatch: 'Passwords must match',
+      passwordVerification:
+        'Password must contain 0-9, a-z, A-Z, ! " # $ % & \' () * + , - . / : ; < = > ? @ [ \\ ] ^ _ ` { | } ~',
+      usernameVerification: 'Password must contain 0-9; A-Z; a-z; _ ; - ',
+    },
+  },
   layout: {
     header: {
       signInBtn: 'Log in',
@@ -7,6 +19,22 @@ export const en = {
   },
   pages: {
     home: 'Home page',
+    signUp: {
+      agreementCheck: {
+        agree: 'I agree to the',
+        and: 'and',
+        privacy: 'Privacy Policy',
+        term: 'Terms of Service',
+      },
+      confirmPassword: 'Password confirmation',
+      email: 'Email',
+      formTitle: 'Sign Up',
+      password: 'Password',
+      questionAboutAccount: 'Do you have an account?',
+      signInBtn: 'Sign Up',
+      signUpLink: 'Sign In',
+      username: 'Username',
+    },
   },
 }
 export type LocaleType = typeof en
--- locales/ru.ts
+++ locales/ru.ts
@@ -1,6 +1,19 @@
 import { LocaleType } from './en'
 
 export const ru: LocaleType = {
+  errors: {
+    signUp: {
+      emailVerification: 'Электронная почта должна соответствовать формату example@example.com',
+      maxPasswordLength20: 'Максимальное количество символов 20',
+      maxPasswordLength30: 'Максимальное количество символов 30',
+      minPasswordLength: 'Минимальное количество символов 6',
+      passwordMismatch: 'Пароли должны совпадать',
+      passwordVerification:
+        'Пароль должен содержать 0-9, a-z, A-Z, ! " # $ % & \' () * + , - . / : ; < = > ? @ [ \\ ] ^ _ ` { | } ~',
+      usernameVerification: 'Пароль может содержать 0-9; A-Z; a-z; _ ; - ',
+    },
+  },
+
   layout: {
     header: {
       signInBtn: 'Войти',
@@ -9,5 +22,21 @@ export const ru: LocaleType = {
   },
   pages: {
     home: 'Главная страница',
+    signUp: {
+      agreementCheck: {
+        agree: 'Я согласен с',
+        and: 'и',
+        privacy: 'политикой конфиденциальности',
+        term: 'условиями предоставления услуг',
+      },
+      confirmPassword: 'Подтверждение пароля',
+      email: 'Электронная почта',
+      formTitle: 'Регистрация',
+      password: 'Пароль',
+      questionAboutAccount: 'У вас есть аккаунт?',
+      signInBtn: 'Зарегистрироваться',
+      signUpLink: 'Войти',
+      username: 'Имя пользователя',
+    },
   },
 }
