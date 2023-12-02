dc5bbd5c07091056087721682d8aea1fa25e89e6
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2023-12-02 15:39:33 +0100

    I1-78 ref: replace static text with locales

--- src/widgets/ConfirmedEmail/ui/ConfirmedEmail.tsx
+++ src/widgets/ConfirmedEmail/ui/ConfirmedEmail.tsx
@@ -1,22 +1,26 @@
 import { EmailConfirmedIcon } from '@/shared/assets'
 import { ROUTES_URL } from '@/shared/const'
+import { useTranslation } from '@/shared/lib/hooks'
 import { Button, Typography } from '@/shared/ui'
 import Link from 'next/link'
 
 import s from './ConfirmedEmail.module.scss'
 
 export const ConfirmedEmail = () => {
+  const { text } = useTranslation()
+  const t = text.pages.confirmedEmail
+
   return (
     <div className={s.wrapper}>
       <div className={s.title}>
-        <Typography variant={'h1'}>Congratulations!</Typography>
+        <Typography variant={'h1'}>{t.title}</Typography>
       </div>
       <Typography className={s.description} variant={'regularText16'}>
-        Your email has been confirmed
+        {t.description}
       </Typography>
       <div className={s.button}>
         <Button as={Link} href={ROUTES_URL.SIGN_IN} variant={'primary'}>
-          Sign In
+          {t.signInBtn}
         </Button>
       </div>
       <EmailConfirmedIcon />
