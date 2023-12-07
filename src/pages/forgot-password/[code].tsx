eb445aead6226aacdaef0f69206a25ed392a8c57
vladnen97 <darkdescent1991@gmail.com>
2023-12-07 10:29:54 +0300

    feat: add pages with code in URI

---
+++ src/pages/forgot-password/[code].tsx
@@ -0,0 +1,8 @@
+import { getRootLayout } from '@/shared/ui'
+
+const CreateNewPassword = () => {
+  return <h1>Forgot Password with code in uri from server</h1>
+}
+
+CreateNewPassword.getLayout = getRootLayout
+export default CreateNewPassword
---
+++ src/pages/sign-up/[code].tsx
@@ -0,0 +1,8 @@
+import { getRootLayout } from '@/shared/ui'
+
+const EmailVerification = () => {
+  return <h1>email verification page</h1>
+}
+
+EmailVerification.getLayout = getRootLayout
+export default EmailVerification
