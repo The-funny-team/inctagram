07a9cb5490ebfd6299efeb93511cfbfeebe0e368
vladnen97 <darkdescent1991@gmail.com>
2023-12-07 21:36:02 +0300

    fix: fix validation schema

--- src/widgets/CreateNewPassword/service/createNewPasswordSchema.ts
+++ src/widgets/CreateNewPassword/service/createNewPasswordSchema.ts
@@ -2,23 +2,20 @@ import { PASSWORD_PATTERN } from '@/shared/const'
 import { LocaleType } from '@locales/en'
 import { z } from 'zod'
 
-export const createNewPasswordSchema = (transcription: LocaleType['pages']['createNewPassword']) =>
+export const createNewPasswordSchema = (transcription: LocaleType['validation']) =>
   z
     .object({
-      confirmPassword: z
+      confirmPassword: z.string().trim(),
+      password: z
         .string()
-        .min(6, { message: transcription.passwordMinLengthMessage })
-        .max(20, { message: transcription.passwordMaxLengthMessage })
-        .refine(value => PASSWORD_PATTERN.test(value), transcription.passwordVerification),
-      password: z.string().min(6).max(20),
+        .min(6, { message: transcription.minLength6 })
+        .max(20, { message: transcription.maxLength20 })
+        .regex(PASSWORD_PATTERN, transcription.passwordVerification)
+        .trim(),
     })
-    .superRefine(({ confirmPassword, password }, ctx) => {
-      if (confirmPassword !== password) {
-        ctx.addIssue({
-          code: 'custom',
-          message: transcription.passwordError,
-        })
-      }
+    .refine(({ confirmPassword, password }) => password === confirmPassword, {
+      message: transcription.passwordMismatch,
+      path: ['confirmPassword'],
     })
 
 export type CreateNewPasswordSchemaType = z.infer<ReturnType<typeof createNewPasswordSchema>>
