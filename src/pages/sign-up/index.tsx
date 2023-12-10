fd74b8a3faa1d24326f7d845d824f76fbfc21a89
vladnen97 <darkdescent1991@gmail.com>
2023-12-10 20:39:01 +0300

    I1-84 ref(styles): replace paddding form layout to page styles

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -2,7 +2,11 @@ import { getRootLayout } from '@/shared/ui'
 import { SignUp } from '@/widgets/SignUp'
 
 const SignUpPage = () => {
-  return <SignUp />
+  return (
+    <div style={{ padding: '36px 10px 0' }}>
+      <SignUp />
+    </div>
+  )
 }
 
 SignUpPage.getLayout = getRootLayout
