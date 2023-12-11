dd7e18292696811c80afe0027b88d11b40309431
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-11 18:45:55 +0300

    I1-85 refactor: refactor SignInPage, add wrapper and style for it

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -1,7 +1,12 @@
 import { getRootLayout } from '@/shared/ui'
 import { SignIn } from '@/widgets/SignIn'
 
-const SignInPage = () => <SignIn />
+import s from './SignInPage.module.scss'
+const SignInPage = () => (
+  <div className={s.root}>
+    <SignIn />
+  </div>
+)
 
 SignInPage.getLayout = getRootLayout
 export default SignInPage
