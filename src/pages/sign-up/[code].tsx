dce19af2b82a21ef62443f35649f7dd40203f1c2
Sergey <Zaikouski.sergey@gmail.com>
2023-12-12 14:48:46 +0200

    I1-86 refactor: remove unnecessary widget, use isSuccess from request

--- src/pages/sign-up/[code].tsx
+++ src/pages/sign-up/[code].tsx
@@ -1,8 +1,29 @@
+import { useEffect } from 'react'
+
+import { useConfirmationMutation } from '@/shared/api/authApi'
 import { getRootLayout } from '@/shared/ui'
-import { EmailVerification } from '@/widgets/EmailVerification'
+import { ConfirmedEmail } from '@/widgets/ConfirmedEmail'
+import { ExpiredLink } from '@/widgets/ExpiredLink'
+import { useRouter } from 'next/router'
 
 const EmailVerificationPage = () => {
-  return <EmailVerification />
+  const router = useRouter()
+
+  const { code } = router.query
+
+  const [confirmation, { isSuccess }] = useConfirmationMutation()
+
+  useEffect(() => {
+    if (code && typeof code === 'string') {
+      confirmation({ code })
+    }
+  }, [code, confirmation])
+
+  const sendEmail = () => {
+    console.log('send request')
+  }
+
+  return <>{isSuccess ? <ConfirmedEmail /> : <ExpiredLink sendEmailHandler={sendEmail} />}</>
 }
 
 EmailVerificationPage.getLayout = getRootLayout
