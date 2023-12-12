57a1dd15c47fe232e50c4bffe69734a0922b01e7
vladnen97 <darkdescent1991@gmail.com>
2023-12-12 18:46:23 +0300

    I1-89 feat: implement PP page

--- src/pages/privacy-policy/index.tsx
+++ src/pages/privacy-policy/index.tsx
@@ -1,7 +1,40 @@
-import { getRootLayout } from '@/shared/ui'
+import { ArrowLeftIcon } from '@/shared/assets'
+import { ROUTES_URL } from '@/shared/const'
+import { useTranslation } from '@/shared/lib/hooks'
+import { Typography, getRootLayout } from '@/shared/ui'
+import Link from 'next/link'
+
+import s from './PrivacyPolicy.module.scss'
 
 const PrivacyPolicy = () => {
-  return <h1>Privacy Policy</h1>
+  const {
+    text: {
+      pages: { privacyPolicy },
+    },
+  } = useTranslation()
+
+  return (
+    <div className={s.root}>
+      <div className={s.Btn}>
+        <ArrowLeftIcon />
+        <Typography
+          as={Link}
+          className={s.link}
+          href={ROUTES_URL.SIGN_UP}
+          variant={'regularText14'}
+        >
+          {privacyPolicy.backToBtn}
+        </Typography>
+      </div>
+
+      <Typography as={'h1'} className={s.title} variant={'h1'}>
+        {privacyPolicy.title}
+      </Typography>
+      <Typography className={s.desc} variant={'regularText14'}>
+        {privacyPolicy.descripton}
+      </Typography>
+    </div>
+  )
 }
 
 PrivacyPolicy.getLayout = getRootLayout
