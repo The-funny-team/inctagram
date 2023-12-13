4998a88cba620c2069cc08e9eab67179d195f02d
vladnen97 <darkdescent1991@gmail.com>
2023-12-13 20:46:31 +0300

    fix: remove non-existent attribute

--- src/widgets/ExpiredLink/ui/ExpiredLink.stories.tsx
+++ src/widgets/ExpiredLink/ui/ExpiredLink.stories.tsx
@@ -10,8 +10,4 @@ const meta = {
 
 export default meta
 type Story = StoryObj<typeof meta>
-export const Default: Story = {
-  args: {
-    email: 'example@example.com',
-  },
-}
+export const Default: Story = {}
