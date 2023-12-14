874afbebeaef5802e1416bcb83812affd1c27a84
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2023-12-14 16:40:26 +0100

    add styles to not-found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -3,15 +3,19 @@ import { Button, Typography, getRootLayout } from '@/shared/ui'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import s from './NotFound.module.scss'
+
 const NotFoundPage = () => {
   const { text } = useTranslation()
   const t = text.pages.notFound
 
   return (
-    <div>
+    <div className={s.wrapper}>
       <Image alt={'not-found'} height={192} src={'/notFound.png'} width={451} />
       <div>
-        <Typography variant={'regularText16'}>{t.title}</Typography>
+        <Typography className={s.title} variant={'regularText16'}>
+          {t.title}
+        </Typography>
       </div>
       <Button as={Link} fullWidth={false} href={'/'}>
         {t.captionBtn}
