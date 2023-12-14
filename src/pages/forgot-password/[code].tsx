8603d80bd003d9ff44f2e0cc08a1dc74249d7bb7
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2023-12-14 10:59:29 +0100

    I1-88 feat: check validation code in uri from server

--- src/pages/forgot-password/[code].tsx
+++ src/pages/forgot-password/[code].tsx
@@ -1,8 +1,40 @@
-import { getRootLayout } from '@/shared/ui'
+import { useEffect, useState } from 'react'
 
-const CreateNewPassword = () => {
-  return <h1>Forgot Password with code in uri from server</h1>
+import { getRootLayout } from '@/shared/ui'
+import { CreateNewPassword } from '@/widgets/CreateNewPassword'
+import { ExpiredLink } from '@/widgets/ExpiredLink'
+import { useParams } from 'next/navigation'
+
+const CreateNewPasswordPage = () => {
+  const [isSuccess, setIsSuccess] = useState(false)
+  const params = useParams()
+  const code = params?.code as string
+
+  const requestCodeFromApi = () => {
+    return '90c87f21-5085-4ce7-be3b-493c8c58053d' // temporary test code
+  }
+
+  useEffect(() => {
+    const codeFromApi = requestCodeFromApi()
+
+    if (code && code === codeFromApi) {
+      setIsSuccess(true)
+    }
+  }, [code])
+  const sendEmail = () => {
+    alert('send request')
+  }
+
+  return (
+    <>
+      {isSuccess ? (
+        <CreateNewPassword code={code} />
+      ) : (
+        <ExpiredLink resendEmailHandler={sendEmail} />
+      )}
+    </>
+  )
 }
 
-CreateNewPassword.getLayout = getRootLayout
-export default CreateNewPassword
+CreateNewPasswordPage.getLayout = getRootLayout
+export default CreateNewPasswordPage
