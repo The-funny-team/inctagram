b3fafbd654b35c56c19b5c89de5e5fee9b52cd15
Sergey <Zaikouski.sergey@gmail.com>
2023-12-19 14:50:40 +0200

    I1-97-1 feat: add ToastContainer to layout

--- src/shared/ui/RootLayout/RootLayout.tsx
+++ src/shared/ui/RootLayout/RootLayout.tsx
@@ -1,9 +1,12 @@
 import React, { PropsWithChildren, ReactElement } from 'react'
+import { ToastContainer } from 'react-toastify'
 
 import { Header } from '@/widgets/Header'
 import { NextPage } from 'next'
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 import s from './RootLayout.module.scss'
 
@@ -12,6 +15,19 @@ const inter = Inter({ subsets: ['latin'] })
 export const RootLayout: NextPage<PropsWithChildren<any>> = ({ children }) => {
   return (
     <>
+      <ToastContainer
+        autoClose={3000}
+        closeOnClick
+        draggable={false}
+        hideProgressBar
+        limit={3}
+        newestOnTop
+        pauseOnFocusLoss={false}
+        pauseOnHover={false}
+        position={'top-center'}
+        rtl={false}
+        theme={'colored'}
+      />
       <Head>
         <title>Next App</title>
         <meta content={'width=device-width, initial-scale=1'} name={'viewport'} />
