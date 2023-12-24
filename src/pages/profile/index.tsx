8560e489d9cfcd659f831cb5d5b17aaafe1f3986
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-25 00:22:41 +0300

    I1-113 feat: add a placeholder for the Profile page

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,10 @@
+import { getRootLayout } from '@/shared/ui'
+
+const ProfilePage = () => (
+  <div>
+    <h1>Profile Page</h1>
+  </div>
+)
+
+ProfilePage.getLayout = getRootLayout
+export default ProfilePage
