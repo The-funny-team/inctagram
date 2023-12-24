9f7f3bf09e6e6c9d25d20c1b6b30cf3be1ff011c
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-24 14:36:27 +0300

    I1-107 feat: add stories for Avatar component

---
+++ src/shared/ui/Avatar/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar } from './Avatar'
+
+const meta = {
+  component: Avatar,
+  tags: ['autodocs'],
+  title: 'Components/Avatar',
+} satisfies Meta<typeof Avatar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const AvatarWithImage: Story = {
+  args: {
+    size: 'large',
+    src: 'https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg',
+    userName: 'Don Joe',
+  },
+}
+
+export const AvatarWithoutImage: Story = {
+  args: {
+    size: 'large',
+    userName: 'Don Joe',
+  },
+}
