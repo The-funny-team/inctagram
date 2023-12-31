5f4d3d9ed418542a74d8b0070d0251c2735b6a9b
VitaliKozhurov <vitalik24021992@gmail.com>
2023-12-31 12:37:47 +0300

    I1-107 refactor: refactor Avatar component with size prop as number value

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -6,22 +6,25 @@ import s from './Avatar.module.scss'
 
 type Props = {
   className?: string
-  size: 'large' | 'small'
+  size: number
   src?: string
   userName: string
 }
 export const Avatar = ({ className, size, src, userName }: Props) => {
   const classNames = {
-    image: s.image,
-    root: clsx(s.root, s[size], className),
+    root: clsx(s.root, className),
     userName: s.userName,
   }
   const fallbackUserName = getFallBackUserName(userName)
 
   return (
-    <div className={classNames.root}>
-      {!src && <span className={classNames.userName}>{fallbackUserName}</span>}
-      {src && <Image alt={'User avatar'} className={classNames.image} fill src={src} />}
+    <div className={classNames.root} style={{ height: `${size}px`, width: `${size}px` }}>
+      {!src && (
+        <span className={classNames.userName} style={{ fontSize: `${10 + size / 8}px` }}>
+          {fallbackUserName}
+        </span>
+      )}
+      {src && <Image alt={`${userName} avatar`} height={size} src={src} width={size} />}
     </div>
   )
 }
