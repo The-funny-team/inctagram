f5ad72a0f4941ca6818376324252a847aac34061
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-01-04 20:46:20 +0100

    I1-105 ref: change mapDays and styles

--- src/shared/ui/DatePicker/services/mapDays.ts
+++ src/shared/ui/DatePicker/services/mapDays.ts
@@ -1,11 +1,5 @@
 import { DateObject } from 'react-multi-date-picker'
 
-const currentDate = {
-  day: new Date().getDate(),
-  month: new Date().getMonth(),
-  year: new Date().getFullYear(),
-}
-
 type MapDaysPropsType = {
   className: string
   currentMonth: any
@@ -15,65 +9,15 @@ type MapDaysPropsType = {
   today: DateObject
 }
 
-const customStyles = {
-  general: {
-    fontSize: '16px',
-    lineHeight: '24px',
-  },
-  today: {
-    backgroundColor: 'transparent',
-    boxShadow: 'none',
-    fontWeight: '700',
-    height: '28px',
-    padding: '4px',
-    width: '28px',
-  },
-}
-
-export const mapDays = ({
-  currentMonth,
-  date,
-  isSameDate,
-  selectedDate,
-  today,
-}: MapDaysPropsType) => {
+export const mapDays = ({ date, today }: MapDaysPropsType) => {
   const props = {
+    className: '',
     style: {},
   }
   const isWeekend = [0, 6].includes(date.weekDay.index)
-  const curDate =
-    date.day === today.day &&
-    currentMonth.index === currentDate.month &&
-    currentDate.year === date.year
-
-  props.style = customStyles.general
-
-  if (isSameDate(date, today)) {
-    props.style = {
-      ...props.style,
-      ...customStyles.today,
-    }
-  }
-
-  if (isSameDate(date, today)) {
-    props.style = {
-      ...props.style,
-      ...customStyles.today,
-    }
-  }
 
   if (isWeekend) {
-    props.style = {
-      ...props.style,
-      color: 'var(--color-danger-300)',
-    }
-  }
-
-  if (!isWeekend) {
-    props.style = {
-      ...props.style,
-      color: curDate ? 'var(--color-accent-500)' : '#FFFFFF',
-    }
+    props.className = 'weekend'
   }
 
   if (date.toDays() < today.toDays() && !isWeekend) {
