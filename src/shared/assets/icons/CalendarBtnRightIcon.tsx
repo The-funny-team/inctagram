9de19ded98ad3a99f03ab3e9943341b885fc9f37
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-01-04 16:15:35 +0100

    I1-105 ref: add ref to props in svg CalendarButtons icons

--- src/shared/assets/icons/CalendarBtnRightIcon.tsx
+++ src/shared/assets/icons/CalendarBtnRightIcon.tsx
@@ -4,6 +4,7 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
   <svg
     fill={'none'}
     height={'24'}
+    ref={ref}
     viewBox={'0 0 24 24'}
     width={'24'}
     xmlns={'http://www.w3.org/2000/svg'}
