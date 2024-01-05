b95e7847a7441c44ae895e846d851406bcf5681e
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-01-05 18:50:51 +0100

    I1-120 feat: add TabsLayout

---
+++ src/shared/ui/TabsLayout/TabsLayout.tsx
@@ -0,0 +1,79 @@
+import { PropsWithChildren, ReactElement, useEffect, useRef, useState } from 'react'
+
+import { ROUTES_URL } from '@/shared/const'
+import { RootLayout, Tabs } from '@/shared/ui'
+import { NextPage } from 'next'
+import { useRouter } from 'next/router'
+
+enum SettingsTabIds {
+  Devices = 'devices',
+  General = 'general',
+  Management = 'management',
+  Payments = 'payments',
+}
+
+const tabsUrlMapper = {
+  [SettingsTabIds.Devices]: ROUTES_URL.DEVICES,
+  [SettingsTabIds.General]: ROUTES_URL.GENERAL,
+  [SettingsTabIds.Management]: ROUTES_URL.MANAGEMENT,
+  [SettingsTabIds.Payments]: ROUTES_URL.PAYMENTS,
+}
+
+interface SettingTab {
+  label: string
+  value: SettingsTabIds
+}
+
+const OPTIONS_TABS: SettingTab[] = [
+  { label: 'General information', value: SettingsTabIds.General },
+  { label: 'Devices', value: SettingsTabIds.Devices },
+  { label: 'Account management', value: SettingsTabIds.Management },
+  { label: 'My payments', value: SettingsTabIds.Payments },
+]
+
+export const TabsLayout: NextPage<PropsWithChildren<{}>> = ({ children }) => {
+  const [value, setValue] = useState<SettingsTabIds | undefined>(undefined)
+  const isFirstInitRef = useRef(false)
+  const router = useRouter()
+
+  const onChangeHandler = () => {
+    const _value = value as SettingsTabIds
+
+    setValue(_value)
+    void router.push(tabsUrlMapper[_value])
+  }
+
+  useEffect(() => {
+    if (isFirstInitRef.current) {
+      return
+    }
+    isFirstInitRef.current = true
+
+    const tabTitle = router.pathname.split('/').pop()
+
+    if (tabTitle == null) {
+      return
+    }
+
+    const _tabTitle = tabTitle as SettingsTabIds
+
+    if (tabsUrlMapper[_tabTitle] != null) {
+      setValue(_tabTitle)
+    }
+  }, [router.pathname])
+
+  return (
+    <div>
+      <Tabs onValueChange={onChangeHandler} options={OPTIONS_TABS} value={value} />
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export const getTabsLayout = (page: ReactElement) => {
+  return (
+    <RootLayout>
+      <TabsLayout>{page}</TabsLayout>
+    </RootLayout>
+  )
+}
