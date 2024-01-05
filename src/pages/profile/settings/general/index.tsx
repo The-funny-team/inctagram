851b55bde6e06a163d07c08fa9c04c55a3fc48b4
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-01-05 19:07:29 +0100

    I1-120 feat: add mock pages: devices, general, management, payments

---
+++ src/pages/profile/settings/general/index.tsx
@@ -0,0 +1,10 @@
+import { getTabsLayout } from '@/shared/ui/TabsLayout'
+
+const GeneralPage = () => (
+  <div>
+    <p>General Info Page</p>
+  </div>
+)
+
+GeneralPage.getLayout = getTabsLayout
+export default GeneralPage
---
+++ src/pages/profile/settings/management/index.tsx
@@ -0,0 +1,10 @@
+import { getTabsLayout } from '@/shared/ui/TabsLayout'
+
+const ManagementPage = () => (
+  <div>
+    <p>Management Page</p>
+  </div>
+)
+
+ManagementPage.getLayout = getTabsLayout
+export default ManagementPage
