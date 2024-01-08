8c6325711fa1c950c05c4409665092c61bb40073
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-01-09 00:07:49 +0100

    I1-120 ref: revert styles for NavBarLayout, refactor TabsLayout

--- src/shared/ui/TabsLayout/TabsLayout.tsx
+++ src/shared/ui/TabsLayout/TabsLayout.tsx
@@ -1,77 +1,38 @@
-import { PropsWithChildren, ReactElement, useEffect, useRef, useState } from 'react'
+import { PropsWithChildren, ReactElement } from 'react'
 
 import { ROUTES_URL } from '@/shared/const'
 import { useTranslation } from '@/shared/lib/hooks'
 import { Tabs } from '@/shared/ui'
 import { WithNavbarLayout } from '@/shared/ui/WithNavbarLayout'
 import { NextPage } from 'next'
-import { useRouter } from 'next/router'
 
 import s from './TabsLayout.module.scss'
 
-enum SettingsTabIds {
-  Devices = 'devices',
-  General = 'general',
-  Management = 'management',
-  Payments = 'payments',
-}
-
-const tabsUrlMapper = {
-  [SettingsTabIds.Devices]: ROUTES_URL.DEVICES,
-  [SettingsTabIds.General]: ROUTES_URL.GENERAL_INFO,
-  [SettingsTabIds.Management]: ROUTES_URL.ACCOUNT_MANAGEMENT,
-  [SettingsTabIds.Payments]: ROUTES_URL.MY_PAYMENTS,
-}
-
-interface SettingTab {
+type optionsTabsType = {
   label: string
-  value: SettingsTabIds
+  value: string
 }
 
 export const TabsLayout: NextPage<PropsWithChildren<{}>> = ({ children }) => {
-  const [value, setValue] = useState<SettingsTabIds | undefined>(undefined)
-  const isFirstInitRef = useRef(false)
-  const router = useRouter()
-  const { text } = useTranslation()
+  const { router, text } = useTranslation()
   const t = text.pages.profile.settings
+  const path = router.pathname
 
-  const optionsTabs: SettingTab[] = [
-    { label: t.general.title, value: SettingsTabIds.General },
-    { label: t.devices.title, value: SettingsTabIds.Devices },
-    { label: t.management.title, value: SettingsTabIds.Management },
-    { label: t.payments.title, value: SettingsTabIds.Payments },
+  const optionsTabs: optionsTabsType[] = [
+    { label: t.general.title, value: ROUTES_URL.GENERAL_INFO },
+    { label: t.devices.title, value: ROUTES_URL.DEVICES },
+    { label: t.management.title, value: ROUTES_URL.ACCOUNT_MANAGEMENT },
+    { label: t.payments.title, value: ROUTES_URL.MY_PAYMENTS },
   ]
 
   const onChangeHandler = (value: string) => {
-    const _value = value as SettingsTabIds
-
-    setValue(_value)
-    void router.push(tabsUrlMapper[_value])
+    void router.push(value)
   }
-
-  useEffect(() => {
-    if (isFirstInitRef.current) {
-      return
-    }
-    isFirstInitRef.current = true
-
-    const tabTitle = router.pathname.split('/').pop()
-
-    if (tabTitle == null) {
-      return
-    }
-
-    const _tabTitle = tabTitle as SettingsTabIds
-
-    if (tabsUrlMapper[_tabTitle] != null) {
-      setValue(_tabTitle)
-    }
-  }, [router.pathname])
 
   return (
     <div className={s.tabsLayoutWrapper}>
-      <Tabs onValueChange={onChangeHandler} options={optionsTabs} value={value} />
-      <div>{children}</div>
+      <Tabs onValueChange={onChangeHandler} options={optionsTabs} value={path} />
+      {children}
     </div>
   )
 }
