38de6162d8f53396faf7226b5cba27531912221a
vladnen97 <darkdescent1991@gmail.com>
2024-01-11 00:17:53 +0300

    I2-15 feat: add tags and credentials

--- src/shared/api/baseApi/baseApi.ts
+++ src/shared/api/baseApi/baseApi.ts
@@ -2,7 +2,8 @@ import { BASE_API_URL } from '@/shared/const'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_API_URL, credentials: 'include' }),
   endpoints: () => ({}),
   reducerPath: 'baseApi',
+  tagTypes: ['Me'],
 })
