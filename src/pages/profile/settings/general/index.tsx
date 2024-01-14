f38cdad0b2c866dc667c4e7428be4dc00bae89e6
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-14 23:57:07 +0300

    I2-16 feat: add ProfileInfoForm style and add ProfileInfoForm to General Page

--- src/pages/profile/settings/general/index.tsx
+++ src/pages/profile/settings/general/index.tsx
@@ -1,9 +1,10 @@
 import {useMeQuery} from '@/shared/api/profileApi'
 import {getTabsLayout} from '@/shared/ui/TabsLayout'
 import {AddProfilePhoto} from '@/widgets/AddProfilePhoto'
+import { ProfileInfoForm } from '@/widgets/ProfileInfoForm'
+
 
 import s from './GeneralPage.module.scss'
-
 
 const GeneralPage = () => {
     const {data: userInfo} = useMeQuery()
@@ -16,7 +17,7 @@ const GeneralPage = () => {
     return (
         <main className={classNames.page}>
             <div className={s.photoUploader}><AddProfilePhoto avatar={avatar}/></div>
-            <div></div>
+            <ProfileInfoForm />
         </main>
     )
 }
