17e18f50ee53c0893f59918f98f6375f3ae7260f
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-14 22:26:00 +0300

    I2-16 feat: add flex layout for user general info page

--- src/pages/profile/settings/general/index.tsx
+++ src/pages/profile/settings/general/index.tsx
@@ -1,10 +1,19 @@
 import { getTabsLayout } from '@/shared/ui/TabsLayout'
 
-const GeneralPage = () => (
-  <div>
-    <p>General Info Page</p>
-  </div>
-)
+import s from './GeneralPage.module.scss'
+const GeneralPage = () => {
+  const classNames = {
+    page: s.page,
+    photoUploader: s.photoUploader,
+  }
+
+  return (
+    <main className={classNames.page}>
+      <div className={s.photoUploader}></div>
+      <div></div>
+    </main>
+  )
+}
 
 GeneralPage.getLayout = getTabsLayout
 export default GeneralPage
