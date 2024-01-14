1fe26e3b3d185f0d183e41559482d8022068679d
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-14 23:56:10 +0300

    I2-16 feat: add ProfileInfoForm

---
+++ src/widgets/ProfileInfoForm/ui/ProfileInfoForm.tsx
@@ -0,0 +1,103 @@
+import { Controller, useForm } from 'react-hook-form'
+
+import { Input, Select } from '@/shared/ui'
+import { DatePicker } from '@/shared/ui/DatePicker'
+
+import s from './ProfileInfoForm.module.scss'
+
+const countriesOptions: any[] = [
+  { title: 'Belarus', value: 'belarus' },
+  { title: 'Russia', value: 'russia' },
+]
+
+const citiesOptions: any[] = [
+  { title: 'Minsk', value: 'minsk' },
+  { title: 'Moscow', value: 'moscow' },
+]
+
+export const ProfileInfoForm = () => {
+  // const { control } = useForm()
+  const { control } = useForm()
+
+  return (
+    <form className={s.form}>
+      <Controller
+        control={control}
+        name={'userName'}
+        render={({ field, fieldState: { error } }) => (
+          <Input
+            // className={classNames.formInput(error?.message)}
+            error={error?.message}
+            label={'Username*'}
+            type={'text'}
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name={'firstName'}
+        render={({ field, fieldState: { error } }) => (
+          <Input
+            // className={classNames.formInput(error?.message)}
+            error={error?.message}
+            label={'First Name*'}
+            type={'text'}
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name={'lastName'}
+        render={({ field, fieldState: { error } }) => (
+          <Input
+            // className={classNames.formInput(error?.message)}
+            error={error?.message}
+            label={'Last Name*'}
+            type={'text'}
+            {...field}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        name={'dateOfBirth'}
+        render={({ field, fieldState: { error } }) => (
+          <DatePicker
+            error={error?.message}
+            onChange={field.onChange}
+            placeholder={'Date of birth'}
+            value={field.value}
+          />
+        )}
+      />
+
+      <div className={s.selectWrapper}>
+        <div className={s.select}>
+          <Controller
+            control={control}
+            name={'country'}
+            render={({ field, fieldState: { error } }) => (
+              <Select
+                label={'Select your country'}
+                options={countriesOptions}
+                placeholder={'Country'}
+              />
+            )}
+          />
+        </div>
+        <div className={s.select}>
+          <Controller
+            control={control}
+            name={'city'}
+            render={({ field, fieldState: { error } }) => (
+              <Select label={'Select your city'} options={citiesOptions} placeholder={'City'} />
+            )}
+          />
+        </div>
+      </div>
+      {/*<TextField fullWidth label={'About me'} />*/}
+    </form>
+  )
+}
