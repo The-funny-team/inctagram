16260f31f9523bfb2d401f49723a4420d2a54932
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-01-17 20:50:57 +0100

    I2-13 ref: remove check public pages and useEffect from ProtectedPage

--- src/shared/ui/ProtectedPage/ProtectedPage.tsx
+++ src/shared/ui/ProtectedPage/ProtectedPage.tsx
@@ -1,24 +1,23 @@
-import { PropsWithChildren, useEffect, useLayoutEffect } from 'react'
+import { PropsWithChildren } from 'react'
 
-import { useLazyMeQuery, useMeQuery } from '@/shared/api/profileApi'
+import { useMeQuery } from '@/shared/api/profileApi'
 import { ROUTES_URL } from '@/shared/const'
-import { PUBLIC_PAGES } from '@/shared/const/publicPages'
 import { Loader } from '@/shared/ui'
 import { useRouter } from 'next/router'
 
-const publicRoutes = PUBLIC_PAGES.map(route => route.split('/')[1])
-
 export const ProtectedPage = ({ children }: PropsWithChildren<{}>) => {
-  const { data: profileInfo, isLoading } = useMeQuery()
+  const { data, isLoading } = useMeQuery()
   const router = useRouter()
-  const currentPath = router.pathname.split('/')[1]
-  const publicPath = publicRoutes.some(path => path === currentPath)
 
-  useEffect(() => {
-    if (!profileInfo && !isLoading && !publicPath) {
-      void router.push(ROUTES_URL.SIGN_IN)
-    }
-  }, [profileInfo, router, isLoading, publicPath])
+  if (isLoading) {
+    return <Loader />
+  }
 
-  return <>{isLoading ? <Loader /> : children}</>
+  if (data == null) {
+    void router.push(ROUTES_URL.SIGN_IN)
+
+    return null
+  }
+
+  return <>{children}</>
 }
