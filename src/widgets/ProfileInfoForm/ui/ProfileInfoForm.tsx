a8dbfd44c118795ba32a2582fa2247e8bfc32aa9
VitaliKozhurov <vitalik24021992@gmail.com>
2024-01-19 15:25:21 +0300

    I2-16 refactor: refactor profile form selectors

--- src/widgets/ProfileInfoForm/ui/ProfileInfoForm.tsx
+++ src/widgets/ProfileInfoForm/ui/ProfileInfoForm.tsx
@@ -1,30 +1,33 @@
 import { Controller, useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 
 import { COUNTRIES_LIST } from '@/shared/const'
 import { Button, Input, Select, TextField } from '@/shared/ui'
 import { DatePicker } from '@/shared/ui/DatePicker'
+import { UserProfileFormValuesType } from '@/widgets/ProfileInfoForm/services/useProfileForm'
 
 import s from './ProfileInfoForm.module.scss'
 
-import { useProfileData, useProfileForm } from '../services'
-
-const citiesOptions: any[] = [
-  { title: 'Minsk', value: 'minsk' },
-  { title: 'Moscow', value: 'moscow' },
-]
+import { useGetCity, useProfileData, useProfileForm } from '../services'
+import { ProfileSelectChildren } from '../ui/ProfileSelectChildren'
 
 export const ProfileInfoForm = () => {
-  const { formData, formErrorsText, formText } = useProfileData()
-  const {
-    control,
-    formState: { errors },
-    handleSubmit,
-    setValue,
-    watch,
-  } = useProfileForm(formData, formErrorsText)
+  const { formData, formErrorsText, formText, profileNotifications, updateProfile } =
+    useProfileData()
+  const { control, handleSubmit, setValue, watch } = useProfileForm(formData, formErrorsText)
+  const country = watch('country')
+
+  const { citiesLoading, citiesOptions } = useGetCity({ country, setValue })
+
+  const onSubmitHandler = (data: UserProfileFormValuesType) => {
+    updateProfile(data)
+      .unwrap()
+      .then(() => toast.success(profileNotifications.successfulSave))
+      .catch(() => toast.success(profileNotifications.errorSave))
+  }
 
   return (
-    <form className={s.form}>
+    <form className={s.form} onSubmit={handleSubmit(onSubmitHandler)}>
       <Controller
         control={control}
         name={'username'}
@@ -84,13 +87,16 @@ export const ProfileInfoForm = () => {
           <Controller
             control={control}
             name={'country'}
-            render={({ field, fieldState: { error } }) => (
+            render={({ field }) => (
               <Select
                 label={formText.countrySelectLabel}
+                onValueChange={field.onChange}
                 options={COUNTRIES_LIST}
                 placeholder={formText.countrySelectPlaceholder}
                 value={field.value}
-              />
+              >
+                <ProfileSelectChildren options={COUNTRIES_LIST} />
+              </Select>
             )}
           />
         </div>
@@ -98,14 +104,20 @@ export const ProfileInfoForm = () => {
           <Controller
             control={control}
             name={'city'}
-            render={({ field, fieldState: { error } }) => (
-              <Select
-                label={formText.citySelectLabel}
-                options={citiesOptions}
-                placeholder={formText.citySelectPlaceholder}
-                value={field.value}
-              />
-            )}
+            render={({ field }) => {
+              return (
+                <Select
+                  disabled={citiesLoading}
+                  label={formText.citySelectLabel}
+                  onValueChange={field.onChange}
+                  options={citiesOptions}
+                  placeholder={formText.citySelectPlaceholder}
+                  value={field.value}
+                >
+                  <ProfileSelectChildren options={citiesOptions} />
+                </Select>
+              )
+            }}
           />
         </div>
       </div>
