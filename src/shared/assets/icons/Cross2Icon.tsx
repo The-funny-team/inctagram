21bd767604e117f017644bfa731115d5d2fa9980
vladnen97 <darkdescent1991@gmail.com>
2024-02-11 01:40:55 +0300

    I2-31 feat: add icons

--- src/shared/assets/icons/Cross2Icon.tsx
+++ src/shared/assets/icons/Cross2Icon.tsx
@@ -2,7 +2,7 @@ import { Ref, SVGProps, forwardRef, memo } from 'react'
 
 const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
-    fill={'none'}
+    fill={'currentColor'}
     height={'14'}
     ref={ref}
     viewBox={'0 0 14 14'}
@@ -14,7 +14,6 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) =
       d={
         'M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z'
       }
-      fill={'white'}
     />
   </svg>
 )
